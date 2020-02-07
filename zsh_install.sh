34dcd96e11b47a9c21775f1fe41045e388f90f3a
DonghunSeol <ssalssi1@gmail.com>
2020-02-07 09:54:22 +0900

    initial commit

---
+++ zsh_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install -y zsh
+sudo apt-get install -y git
+rm -rf /root/.oh-my-zsh 
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Setting zsh theme as "agonoster"
+sed -i 's/ZSH_THEME=".*/ZSH_THEME="agnoster"/' ~/.zshrc
