6050d9e52b23084be080ef165d37c43eaac4449b
Donghun Seol <47937914+atoye1@users.noreply.github.com>
2020-02-08 16:58:44 +0900

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y git
 rm -rf /root/.oh-my-zsh 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Setting zsh theme as "agonoster"
+echo "edit ~/.zshrc to change THEME"
 sed -i 's/ZSH_THEME=".*/ZSH_THEME="agnoster"/' ~/.zshrc
